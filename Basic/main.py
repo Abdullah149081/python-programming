a8aad8cc6207bea6ecbf12ed74f317d86da7888a
Abdullah149081 <abdullah149081@hotmail.com>
2025-03-24 11:53:42 +0600

    python

---
+++ Basic/main.py
@@ -0,0 +1,37 @@
+age = 10
+str_name = "Masud"
+print(age)
+print(str_name)
+
+#  type checking
+print(type(age), type(str_name))
+
+# typecasting
+a = str(age)
+print(type(a), type(age))
+
+# template literal
+name = f"My name is {str_name} ,My age {age}."
+print(name)
+
+# python by default input str
+user_Input = input()
+user_Age = input()
+
+print("Your name", user_Input, "Your age", user_Age)
+
+# operator
+print(106 / 3)
+print(106 % 3)
+print(106 // 3)  # purno vag fol
+
+
+for i in range(1, 10, 2):
+    print(i, end=" ")
+
+print("\n", end="")
+
+num = [1, 2, 3, 4, 5]
+
+for idx, element in enumerate(num):
+    print(idx, element)
